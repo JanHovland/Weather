11fe82b30d02d79205165645e028d881083c9472
Jan Hovland <jan.hovland@lyse.net>
2021-05-13 19:08:44 +0200

    Update

--- Weather/City Views/CityRowView.swift
+++ Weather/City Views/CityRowView.swift
@@ -21,19 +21,24 @@ struct CityRowView: View {
             if cityRecord.icon != "000" {
                 Text(cityRecord.city)
                 Spacer()
-                Image("Arrow_north")
-                    .resizable()
-                    .frame(width: 50 , height: 50, alignment: .center)
-                    .rotationEffect(Angle(degrees: Double(cityRecord.deg)), anchor: .center)
-                    .padding(.bottom, -5)
                 Text("\(Int(round(cityRecord.temp)))")
                     .modifier(ForeGroundColor(temp: cityRecord.temp))
                 Text(" º C")
                 Image(cityRecord.icon)
                     .resizable()
                     .frame(width: 40, height: 40, alignment: .center)
+                Image("Arrow_north")
+                    .resizable()
+                    .frame(width: 50 , height: 50, alignment: .center)
+                    .rotationEffect(Angle(degrees: Double(cityRecord.deg)), anchor: .center)
+                    .padding(.bottom, -5)
+                
+                let msg1 = String(format:"%.0f", cityRecord.wind_speed)
+                let msg2 = " (" + String(format:"%.0f", cityRecord.wind_gust!) + ")"
+                Text(msg1 + msg2)
             }
         }
+        .font(.system(size: 16, weight: .regular))
     }
 }
 
